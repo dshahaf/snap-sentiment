058ff6e3a36227bd6335efd921bcf42dde0dde88
James Nam <namejames91@gmail.com>
2013-10-21 12:17:29 -0700

    form;

--- app1/views.py
+++ app1/views.py
@@ -1,7 +1,12 @@
 # Create your views here.
 from django.shortcuts import render
 from django.template import RequestContext, loader
+from django import forms
 
 def index(request):
 	context = {}
+	if request.method == 'POST':
+		form = request.POST
+		value = form.get('textarea')
+		context = {'orig': value, 'result': 'hahaha'}
 	return render(request, 'word.html', context)
