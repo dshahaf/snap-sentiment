8f3d5cc075a4b3d3386188121e0e5579857927df
James Nam <namejames91@gmail.com>
2013-10-20 22:06:19 -0700

    template dir;

--- sentiment/settings.py
+++ sentiment/settings.py
@@ -1,5 +1,7 @@
 # Django settings for sentiment project.
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -111,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/Users/jnam/Developer/sentiment-app/sentiment/templates",
+    os.path.join(__file__, "../templates")
 )
 
 INSTALLED_APPS = (
