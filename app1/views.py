de1542ca5356002ec17d8828a5554a93d41e6afd
James Nam <namejames91@gmail.com>
2013-10-21 13:22:22 -0700

    need to work on logic for word analysis;

--- app1/views.py
+++ app1/views.py
@@ -3,10 +3,48 @@ from django.shortcuts import render
 from django.template import RequestContext, loader
 from django import forms
 
+"""
+result = {
+	'words': [
+		{'value' : 'love', 'sentiment' : 'positive'},
+		{'value' : 'is', 'sentiment' : 'neither'},
+		{'value' : 'bad', 'sentiment' : 'negative'},
+		{'value' : 'love', 'sentiment' : 'positive'},
+		{'value' : 'is', 'sentiment' : 'neither'},
+		{'value' : 'good', 'sentiment' : 'positive'},
+	],
+	'overall_sentiment' : 'positive',
+	'sentimental_words': {
+		'positive': [
+			{
+				'value' : 'love',
+				'count' : 2
+			},
+			{
+				'value' : 'good',
+				'count' : 1
+			},
+				
+		],
+		'negative': [
+			{
+				'value' : 'bad',
+				'count' : 1
+			},	
+		],				
+		'count': {
+			'positive' : 3,
+			'negative' : 1,
+		}
+	},
+}
+"""
+
 def index(request):
 	context = {}
 	if request.method == 'POST':
 		form = request.POST
 		value = form.get('textarea')
-		context = {'orig': value, 'result': 'hahaha'}
+		result = {}
+		context = {'text': value, 'result': result}
 	return render(request, 'word.html', context)
