b1de6191161bdc4e748fe2b4112b557166343848
James Nam <namejames91@gmail.com>
2013-10-22 02:55:36 -0700

    working on noun based, tokenization;

--- app2/views.py
+++ app2/views.py
@@ -2,6 +2,20 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
 
+"""
+{
+	'text' : ""
+	'result' : {
+		'words': []
+	}
+}
+"""
 def index(request):
 	context = {}
+	if request.method == 'POST':
+		form = request.POST
+		value = form.get('textarea')
+		context['text'] = value
+		context['result'] = {}
+		context['result']['words'] = value.split()
 	return render(request, 'noun.html', context)
