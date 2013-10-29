e1457747468b1e6daa364aebe0fb2b0866e68d07
James Nam <namejames91@gmail.com>
2013-10-28 23:27:08 -0700

    sentence analysis html structure done;

--- app_sentence/views.py
+++ app_sentence/views.py
@@ -6,4 +6,15 @@ from engine.sentiment_analysis import SentimentAnalysis
 
 def index(request):
 	context = {}
+
+	if request.method == 'POST':
+		form = request.POST
+		action = form.get('action')
+		context['action'] = action
+
+		if action == 'user-input':
+			value = form.get('textarea')
+			context['text'] = value
+			context['result'] = value
+
 	return render(request, 'sentence.html', context)
