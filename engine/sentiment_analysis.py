9e571821fa743fc4edcab0152e1a1ce57d910166
James Nam <namejames91@gmail.com>
2013-11-14 22:56:34 -0800

    new k scoring done;

--- engine/sentiment_analysis.py
+++ engine/sentiment_analysis.py
@@ -4,7 +4,7 @@ import nltk, os
 from nltk import pos_tag
 from text_processor import TextProcessor
 from corpus import Corpus
-from math import log
+from math import log, sqrt
 from lib.porter2 import stem
 
 class SentimentAnalysis:
@@ -247,7 +247,26 @@ class SentimentAnalysis:
 		else:
 			return (b, a)
 
-	def getControversyScoreFromCounts(self, posCount, negCount, scaleFactor = 2):
+	def getControversyScoreFromCounts(self, posCount, negCount):
+		cacheKey = self.getOrderedPair(posCount, negCount)
+		if cacheKey in self.controversyScoreCache:
+			return self.controversyScoreCache[cacheKey]
+
+		p = posCount
+		n = negCount
+
+		s = posCount + negCount # sum
+		sN = s + 1 # sum Normalized
+		d = abs(posCount - negCount) # diff
+		dR = 1.0 * d / sN # diff Ratio
+		dF = 1 - dR * dR * (1 - 1.0 / sqrt(sN)) # diff Factor
+
+		ret = log(sN * dF)
+		self.controversyScoreCache[cacheKey] = ret
+		return ret
+
+	# attempt 1
+	def getControversyScoreFromCounts1(self, posCount, negCount, scaleFactor = 2):
 		cacheKey = self.getOrderedPair(posCount, negCount)
 		if cacheKey in self.controversyScoreCache:
 			return self.controversyScoreCache[cacheKey]
